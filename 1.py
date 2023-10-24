14a2930d64ee242cdb8d749e39d8e61f0841aa8a
vikusido <145844166+vikusido@users.noreply.github.com>
2023-10-24 20:06:09 +0300

    добавила фразы и поменяла программу

--- 1.py
+++ 1.py
@@ -1,6 +1,10 @@
 from random import choice
 
-file = open("phrase.txt", "r")
+file = open("phrase ch.txt", "r")
+file1 = open("phrase m.txt", "r")
 data = file.readlines()
 print(choice(data))
 file.close
+data1 = file1.readlines()
+print(choice(data1))
+file1.close
